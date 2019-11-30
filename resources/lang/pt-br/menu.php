73bf37f53c8150b1911b93470860e18209f51537
Kelvin Cluxnei <kelvin.cluxnei@etec.sp.gov.br>
2019-11-30 14:57:57 -0300

    fix admin translate word

--- resources/lang/pt-br/menu.php
+++ resources/lang/pt-br/menu.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'account_settings'              => 'Configurações de Conta',
-    'users' => 'Usuários',
-    'administrators-users' => 'Adminstradores',
-    'cities' => 'Cidades',
-    'courses' => 'Cursos',
-    'companies' => 'Empresas',
-    'jobs' => 'Vagas',
-    'search' => 'Pesquisar',
+    'account_settings'     => 'Configurações de Conta',
+    'users'                => 'Usuários',
+    'administrators-users' => 'Administradores',
+    'cities'               => 'Cidades',
+    'courses'              => 'Cursos',
+    'companies'            => 'Empresas',
+    'jobs'                 => 'Vagas',
+    'search'               => 'Pesquisar',
 ];
