983616c5221ae9e33eb9e630f82c7c35bb903105
Kelvin Cluxnei <kelvin.cluxnei@etec.sp.gov.br>
2019-11-30 16:11:48 -0300

    Add dev user to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,5 +14,14 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(User::class, 100)->create();
+
+        User::create([
+            'name' => 'Developer tester',
+            'email' => 'dev@dev.dev',
+            'password' => Hash::make('dev'),
+            'cpf' => '00123321123',
+            'administrator' => 1,
+            'approved' => 1
+        ]);
     }
 }
